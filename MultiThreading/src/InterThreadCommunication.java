1a84c8d354e2416e9f0412f14f03a37dd849db79
AAMIR SALEEM <aamir.saleem97@gmail.com>
2018-09-20 16:28:21 +0530

    4

---
+++ MultiThreading/src/InterThreadCommunication.java
@@ -0,0 +1,71 @@
+class Q {
+    int n;
+    boolean valueSet = false;
+
+    synchronized int get(){
+        if (!valueSet)
+            try {
+                wait();
+            }catch (InterruptedException e){
+                System.out.println("Interrupted");
+            }
+            System.out.println("Got : " + n);
+            valueSet = false;
+            notify();
+            return n;
+    }
+
+    synchronized void put(int n) {
+        if (valueSet)
+            try {
+                wait();
+            }catch (InterruptedException e){
+                System.out.println("Interrupted");
+            }
+            this.n = n;
+            valueSet = true;
+            System.out.println("Put : " + n);
+            notify();
+    }
+}
+
+class Producer implements Runnable {
+    Q q;
+    Producer(Q q){
+        this.q = q;
+        new Thread(this, "Producer").start();
+    }
+
+    @Override
+    public void run() {
+        int i = 0;
+        while(true) {
+            q.put(i++);
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    Q q;
+    Consumer(Q q){
+        this.q = q;
+        new Thread(this, "Consumer").start();
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            q.get();
+        }
+    }
+}
+
+public class InterThreadCommunication {
+    public static void main(String[] args) {
+        Q q = new Q();
+        new Producer(q);
+        new Consumer(q);
+
+        System.out.println("Press Ctrl-C to Stop");
+    }
+}
